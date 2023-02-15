3ed17084648e627c4fff135336465a4569d938ed
Kenneth Daily <kdaily@amazon.com>
2023-02-15 10:27:01 -0800

    Clean up test file.

--- tests/unit/test_configured_endpoint_url.py
+++ tests/unit/test_configured_endpoint_url.py
@@ -16,9 +16,6 @@ import botocore.session as session
 from botocore.args import (
     ConfiguredEndpointProviderChain,
 )
-from botocore.exceptions import (
-    InvalidConfigError,
-)
 from botocore.model import ServiceModel
 
 from tests import unittest
@@ -36,76 +33,6 @@ def _known_service_names_and_ids():
     return result
 
 
-# class TestLinkedSectionConfigProvider(unittest.TestCase):
-#     def assert_provides_value(
-#         self,
-#         config_file_values,
-#         scoped_config,
-#         linked_section_type,
-#         expected_value,
-#         expected_exception=None,
-#     ):
-
-#         provider = LinkedSectionProvider(
-#             full_config=config_file_values,
-#             scoped_config=scoped_config,
-#             linked_section_type=linked_section_type,
-#         )
-#         if expected_exception is not None:
-#             with pytest.raises(expected_exception):
-#                 value = provider.provide()
-#             return
-
-#         value = provider.provide()
-#         self.assertEqual(value, expected_value)
-
-#     def test_provide_section_config(self):
-#         full_config = {
-#             "profiles": {
-#                 "test": {"services": "my-services"}
-#             },
-#             "services": {
-#                 "my-services": {
-#                     "endpoint_url": "https://global-config-endpoint.aws:1234/",
-#                     "s3": {
-#                         "endpoint_url": "https://s3-config-endpoint.aws:1234/"
-#                     }
-#                 }
-#             }
-#         }
-
-#         expected_section = {
-#             "endpoint_url": "https://global-config-endpoint.aws:1234/",
-#             "s3": {
-#                 "endpoint_url": "https://s3-config-endpoint.aws:1234/"
-#             }
-#         }
-
-#         self.assert_provides_value(
-#             config_file_values=full_config,
-#             scoped_config=full_config['profiles']['test'],
-#             linked_section_type='services',
-#             expected_value=expected_section)
-
-#     def test_provide_service_config_missing_service(self):
-#         self.assert_provides_value(
-#             config_file_values={},
-#             scoped_config={},
-#             linked_section_type='services',
-#             expected_value=None,
-#         )
-
-#     def test_provide_service_config_not_a_section(self):
-#         self.assert_provides_value(
-#             config_file_values={"profiles": {
-#                 "test": {"services": "my-services"}}},
-#             scoped_config={"services": "my-services"},
-#             linked_section_type="services",
-#             expected_value=None,
-#             expected_exception=InvalidConfigError
-#         )
-
-
 class TestCustomEndpointProviderChain(unittest.TestCase):
     def assert_chain_does_provide(
         self,
@@ -116,10 +43,6 @@ class TestCustomEndpointProviderChain(unittest.TestCase):
         scoped_config_map,
         expected_value,
     ):
-        # fake_session = mock.Mock(spec=session.Session)
-        # fake_session.get_config_variable.return_value = 'default'
-        # fake_session.full_config = full_config_map
-        # fake_session.instance_variables.return_value = instance_map
         mocked_service_model = unittest.mock.Mock(spec=ServiceModel)
         mocked_service_model.service_id = service
         mocked_service_model.service_name = service
@@ -152,21 +75,6 @@ class TestCustomEndpointProviderChain(unittest.TestCase):
             scoped_config_map={},
             expected_value=None,
         )
-
-    # TODO: Should probably have an interface more similar to this
-    # where a config factory creator can set the env var names to search for
-    # so that it could look at AWS_ENDPOINT_URL_SERVICE and then AWS_ENDPOINT_URL
-    # without necessarily having to configure that in the class definition
-    # def test_does_provide_value_if_variable_exists_in_env_var_list(self):
-    #     self.assert_chain_does_provide(
-    #         instance_map={},
-    #         environ_map={'FOO': 'bar'},
-    #         scoped_config_map={},
-    #         create_config_chain_args={
-    #             'env_var_names': ['FOO'],
-    #         },
-    #         expected_value='bar',
-    #     )
 
     def test_does_provide_service_value_when_both_env_vars_exist(self):
         self.assert_chain_does_provide(
