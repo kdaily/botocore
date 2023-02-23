1e990d9125c4725a5ae2358bedbac9f5604e59b7
Kenneth Daily <kdaily@amazon.com>
2023-02-22 16:04:51 -0800

    Refactor to improve test readability.
    Move the JSON to a dedicated test cases directory. This models
    the structure of the unit test case data. Having it in a named
    directory of `test-cases` makes it clear what the intention is.

--- tests/functional/test_configured_endpoint_url.py
+++ tests/functional/test_configured_endpoint_url.py
@@ -14,11 +14,11 @@ ENDPOINT_TESTDATA_FILE = Path(__file__).parent / 'data' / "profile-tests.json"
 with open(ENDPOINT_TESTDATA_FILE) as f:
     data = json.load(f)
     suites = data.get('testSuites', {})
-    ENDPOINT_TEST_CASES = suites[1]
+    ENDPOINT_TEST_SUITE = suites[0]
 
 
-def dict_to_ini_section(ini_dict, section_name, name):
-    profile_str = f'[{section_name} {name}]\n'
+def dict_to_ini_section(ini_dict, section_header):
+    profile_str = f'[{section_header}]\n'
     for key, value in ini_dict.items():
         if isinstance(value, dict):
             profile_str += f"{key} =\n"
@@ -29,93 +29,115 @@ def dict_to_ini_section(ini_dict, section_name, name):
     return profile_str + "\n"
 
 
-def create_cases(cases):
-    data = cases
-    profiles = data['profiles']
-    service_sections = data['services']
-    tests = data['endpointUrlTests']
-    new_tests = []
+def create_cases(test_suite):
+    profiles = test_suite['profiles']
+    service_sections = test_suite['services']
+    environments = test_suite['environments']
+    client_configs = test_suite['client_configs']
+    tests = test_suite['endpointUrlTests']
+
     for test in tests:
         new_test = test.copy()
+
+        data_to_add = {}
+        environment_name = test.get('environment', None)
+        environment = environments.get(environment_name, {})
+        data_to_add['environment'] = environment
+
+        client_config_name = test.get('client_config', None)
+        client_config = client_configs.get(client_config_name, {})
+        data_to_add['client_config'] = client_config
+
         profile_name = test.get('profile', None)
         profile = profiles.get(profile_name, {})
+        data_to_add['profile'] = profile
+
         service_section_name = profile.get('services', None)
         service_section = service_sections.get(service_section_name, {})
-        profile_str = dict_to_ini_section(
-            profile, section_name="profile", name=profile_name
-        )
-        service_section_str = dict_to_ini_section(
-            service_section, section_name='services', name=service_section_name
-        )
-        combined_profile_str = profile_str + service_section_str
-        new_test.update({'profile_string': combined_profile_str})
-        new_tests.append(new_test)
-    return new_tests
+        data_to_add['services'] = service_section
+
+        new_test['augmented_data'] = data_to_add
+        yield new_test
 
 
-def parametrize_test_cases(cases):
-    cases = create_cases(cases)
-    return pytest.mark.parametrize(
-        "test_case",
-        cases,
-        ids=[c["name"] for c in cases],
+def _normalize_endpoint(url):
+    split_endpoint = urlsplit(url)
+    actual_endpoint = f"{split_endpoint.scheme}://{split_endpoint.netloc}"
+    return actual_endpoint
+
+
+def assert_endpoint_url_used(client, operation, params, expected_endpoint_url):
+    http_stubber = ClientHTTPStubber(client)
+    http_stubber.start()
+    http_stubber.add_response()
+
+    assert client.meta.endpoint_url == expected_endpoint_url
+
+    # Call an operation on the client
+    getattr(client, operation)(**params)
+
+    assert (
+        _normalize_endpoint(http_stubber.requests[0].url)
+        == expected_endpoint_url
     )
 
 
-@fixture
-def mock_botocore_session():
-    return botocore.session.get_session()
+def idfn(test_case):
+    return test_case['name']
 
 
-def assert_endpoint(request, expected_endpoint):
-    split_endpoint = urlsplit(request.url)
-    print(split_endpoint)
-    actual_endpoint = f"{split_endpoint.scheme}://{split_endpoint.netloc}"
-    # if split_endpoint.path[0] == "/":
-    #     actual_endpoint += split_endpoint.path[0]
-    assert actual_endpoint == expected_endpoint
+def setup_configuration_environment(test_case):
+    profile_str = dict_to_ini_section(
+        test_case['augmented_data']['profile'],
+        section_header=f"profile {test_case['profile']}",
+    )
+    services_section_name = test_case['augmented_data']['profile'].get(
+        'services', None
+    )
+    service_section_str = dict_to_ini_section(
+        test_case['augmented_data']['services'],
+        section_header=f'services {services_section_name}',
+    )
+    test_case['augmented_data']['profile_string'] = (
+        profile_str + service_section_str
+    )
 
 
-@parametrize_test_cases(ENDPOINT_TEST_CASES)
-@pytest.mark.parametrize("client_config", [True, False])
-def test_resolve_custom_endpoint_url(
-    test_case, client_config: bool, mock_botocore_session
-):
-    environment = test_case.get('environment', {})
-
+def get_client(test_case):
+    botocore_session = botocore.session.get_session()
     # need to update the environment with the path to
     # the temp config file
     with temporary_file('w') as f, mock.patch.dict(
         botocore.configprovider.os.environ,
-        dict(**environment, **{"AWS_CONFIG_FILE": f.name}),
+        dict(
+            **test_case['augmented_data']['environment'],
+            **{"AWS_CONFIG_FILE": f.name},
+        ),
         clear=True,
     ) as mockenv:  ## noqa
 
-        f.write(test_case['profile_string'])
+        f.write(test_case['augmented_data']['profile_string'])
         f.flush()
 
-        mock_botocore_session.set_config_variable(
-            'profile', test_case['profile']
+        botocore_session.set_config_variable('profile', test_case['profile'])
+
+        client = botocore_session.create_client(
+            test_case['service'],
+            **test_case['augmented_data']['client_config'],
         )
 
-        mock_botocore_session.set_config_variable(
-            'ignore_config_endpoint_urls', False
-        )
+        return client
 
-        if client_config:
-            output_url = "https://clientconfig.endpoint.aws"
-            client = mock_botocore_session.create_client(
-                test_case['service'], endpoint_url=output_url
-            )
-        else:
-            client = mock_botocore_session.create_client(test_case['service'])
-            output_url = test_case['output']['endpointUrl']
 
-        http_stubber = ClientHTTPStubber(client)
-        http_stubber.start()
-
-        assert client.meta.endpoint_url == output_url
-
-        http_stubber.add_response()
-        client.list_objects(Bucket="foo")
-        assert_endpoint(http_stubber.requests[0], output_url)
+@pytest.mark.parametrize(
+    "test_case", create_cases(ENDPOINT_TEST_SUITE), ids=idfn
+)
+def test_resolve_configured_endpoint_url(test_case):
+    setup_configuration_environment(test_case)
+    client = get_client(test_case)
+    assert_endpoint_url_used(
+        client=client,
+        operation="list_objects",
+        params={"Bucket": "Foo"},
+        expected_endpoint_url=test_case['output']['endpointUrl'],
+    )
